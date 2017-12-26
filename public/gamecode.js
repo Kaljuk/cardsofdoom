d7e85b7ad992a6198ba2cab6c7d34220c198cadc
kaljuk <erik.kaljumae@gmail.com>
2017-12-26 20:45:15 +0200

    Added segmented background creation

--- public/gamecode.js
+++ public/gamecode.js
@@ -5,16 +5,32 @@ var socketlocation = 'http://localhost:1337';
 
 function setup() {
 
-  createCanvas(640,480);
+  createCanvas(720,480);
   background(51);
+  drawBackground(0);
   // Connect to the socket
   socket = io.connect(socketlocation);
   socket.on('mouse', drawnew);
 }
 
+function drawBackground(btype) {
+  noStroke();
+  //      BackgroundSky, BackgroundGround, MainGround, MainUnderGround
+  let colors1= ['#FCFF89', '#FE7187', '#CA4B7C', '#6E386E'];
+  let colorsS= ['#A3F7BF', '#29A19C', '#4B6289', '#60316E'];
+  var colors = colors1.reverse();
+  var bgPerc = [41,75,88,100].reverse();
+  colors.forEach((e, i) => {
+    fill(e);
+    let bgheight = height/100*bgPerc[i];
+    console.log(bgheight, width, height);
+    rect(0,0, width, bgheight);
+  })
+}
+
 function drawnew (data) {
   noStroke();
-  fill(123,123,123);
+  fill('#121212');
   ellipse(data.x, data.y, 15, 15);
 }
 
@@ -22,7 +38,7 @@ function mouseDragged() {
   console.log('MouseXY', mouseX, mouseY);
   if (mouseButton==='left') {
     noStroke();
-    fill(255);
+    fill('#121212');
     ellipse(mouseX, mouseY,50,50);
     var sendData = {
       x:mouseX,
